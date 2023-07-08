c10832dc68ba01b22892ad5aa98f3ddc9a0fdefb
ProveY0urWorth <snowmmodestiny@gmail.com>
2023-07-08 16:39:57 +0300

    Added everything for the start

---
+++ JoyDev_Practice/Helpers/Colors.swift
@@ -0,0 +1,18 @@
+//
+//  Colors.swift
+//  JoyDev_Practice
+//
+//  Created by  ProveY0urW0rth on 08.07.2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    static let active = Color(red: 1/255, green: 79/255, blue: 190/255)
+    static let plain = Color(red: 111/2555, green: 111/255, blue: 111/255)
+    static let background = Color(red: 0/255, green: 0/255, blue: 0/255)
+    //active - rgba(1,79,190,255)
+    //plain - rgba(111,111,111,255)
+    //background - rgba(0,0,0,255)
+}
---
+++ JoyDev_Practice/Profile/ProfileView/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  JoyDev_Practice
+//
+//  Created by  ProveY0urW0rth on 08.07.2023.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).foregroundColor(Color.active)
+        Text("erm").foregroundColor(Color.red)
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
